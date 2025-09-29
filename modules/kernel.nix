2ed6061de15a1f05aaddcace0dd9a3172561d708
Connor Garey <connorgarey1999@gmail.com>
2025-09-29 01:29:47 +0100

    Commented out latest kernel as causing potential GPU underclock issues.

--- modules/kernel.nix
+++ modules/kernel.nix
@@ -2,7 +2,7 @@
 
 {
   # Latest linux kernel:
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # Kernel boot params
   boot.kernelParams = [ "split_lock_detect=off" ];
