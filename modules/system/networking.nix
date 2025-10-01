fb4cc4915cfdc451d559585a66f3ba58a943f76a
Connor Garey <connorgarey1999@gmail.com>
2025-10-01 02:15:56 +0100

    Added allowPing and rejectPackets

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -16,6 +16,10 @@
 
     firewall = {
       enable = true;
+      
+      allowPing = true;
+      rejectPackets = false;
+
       allowedTCPPorts = [
         80      # HTTP
         443     # HTTPS
@@ -24,6 +28,7 @@
         40050   # computer-stat-net
         53317   # localsend
       ];
+      
       allowedUDPPorts = [
         30000   # Luanti
         5069    # System Shock 2 Remastered LAN?
