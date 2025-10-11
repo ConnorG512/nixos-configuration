a82f6df829e168f3ffc56423375b0fd282fb0bcb
Connor Garey <connorgarey1999@gmail.com>
2025-10-11 19:01:43 +0100

    nvf lsp changes

--- modules/apps/nvim-nvf.nix
+++ modules/apps/nvim-nvf.nix
@@ -3,6 +3,8 @@
 {
   programs.nvf = {
     enable = true;
+    enableManpages = true;
+
     settings = {
       vim = {
         luaConfigRC = { };
@@ -18,32 +20,68 @@
           name = "tokyonight";
           style = "night";
         };          
+        lsp = {
+          servers = {
+            "*" = {
+              root_markers = [ ".git" ];
+              capabilities = {
+                textDocument = {
+                  semanticTokens = {
+                    multilineTokenSupport = true;
+                  };
+                };
+              };
+            };
 
-        autocomplete.nvim-cmp.enable = true;
+            "clangd" = {
+              cmd = [ "clangd" ];
+              filetypes = [ "c" "cpp" "h" "hpp" ];
+            };
+            "nil" = {
+              cmd = [ "nil-ls" ];
+              filetypes = [ "nix" "flake" "lock" ];
+            };
+            "zls" = {
+              cmd = [ "zls" ];
+              filetypes = [ "zig" "zig.zon" ];
+            };
+          };
+        };
+        languages = {
+          enableTreesitter = true;
+          enableFormat = true;
+
+          clang = {
+            enable = true;
+          };
+        };
+        autocomplete.nvim-cmp = {
+          enable = true;
+          sourcePlugins = [
+            "cmp-nvim-lsp"
+            "cmp-path"
+            "cmp-treesitter"
+            "cmp-luasnip"
+            "cmp-buffer"
+            "nvim-lspconfig"
+            "vim-markdown"
+            "cheatsheet-nvim"
+            "nvim-treesitter"
+            "comment-nvim"
+            "harpoon"
+            "nix-develop-nvim"
+          ];
+          sources = {
+            buffer = "[Buffer]";
+            nvim_lsp = "[LSP]";
+            path = "[Path]";
+            treesitter = "[Treesitter]";
+            luasnip = "[Snippet]";
+          };
+        };
         autopairs.nvim-autopairs.enable = true;
         statusline.lualine.enable = true;
         telescope.enable = true;
-
-        languages = {
-          enableLSP = true;
-          enableFormat = false;
-          enableTreesitter = true;
-          zig = {
-            enable = true;
-            lsp.enable = true;
-            treesitter.enable = true;
-          };
-          nix = {
-            enable = true;
-            lsp.enable = true;
-            treesitter.enable = true;
-          };
-          clang = {
-            enable = true;
-            lsp.enable = true;
-            treesitter.enable = true;
-          };
-        };
       };
     };
   };
