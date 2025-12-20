d08ebf964b0dc82d9069f82f7a68144d2a4e2bba
Connor Garey <connorgarey1999@gmail.com>
2025-12-20 23:25:45 +0000

    Added ffmpeg and Gstreamer explicitly.

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -66,5 +66,16 @@
     exfat
     nixfmt-rfc-style
     wl-clipboard
+    
+    # ffmpeg
+    ffmpeg_7
+    # Gstreamer Codecs
+    gst_all_1.gstreamer
+    gst_all_1.gst-vaapi
+    gst_all_1.gst-libav
+    gst_all_1.gst-plugins-ugly
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-base
   ];
 }
