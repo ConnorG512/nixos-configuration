6809956cb9ef2ebe6d51c8e9889eba49628bf2a5
Connor Garey <connorgarey1999@gmail.com>
2026-01-15 17:19:59 +0000

    setting force to turn off resolved

--- modules/system/networking/dns.nix
+++ modules/system/networking/dns.nix
@@ -1,12 +1,13 @@
-{config, ...}:
+{config, pkgs, lib, ...}:
 
 {
   # Currently auto detecting DNS from router however sometimes failing and cutting internet.
   # Force use of public DNS addresses and disable auto resolving.
-  services.resolved.enable = false; 
-  netowrking.nameservers = [
-    "8.8.8.8" # Google
+  # resolved is currently forced off, set to true or remove to turn on. 
+  services.resolved.enable = lib.mkForce false; 
+  networking.nameservers = [
     "1.1.1.1" # Cloudflare Primary
     "1.0.0.1" # Cloudflare Secondary
+    "8.8.8.8" # Google
   ];
 }
