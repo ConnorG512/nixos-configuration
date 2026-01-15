e18d41c30d50d2f679e0db6e3ad15ea9579aaddb
Connor Garey <connorgarey1999@gmail.com>
2026-01-15 17:27:09 +0000

    Added turn off automatic dns from the router.

--- modules/system/networking/dns.nix
+++ modules/system/networking/dns.nix
@@ -3,11 +3,19 @@
 {
   # Currently auto detecting DNS from router however sometimes failing and cutting internet.
   # Force use of public DNS addresses and disable auto resolving.
+  
   # resolved is currently forced off, set to true or remove to turn on. 
   services.resolved.enable = lib.mkForce false; 
-  networking.nameservers = [
-    "1.1.1.1" # Cloudflare Primary
-    "1.0.0.1" # Cloudflare Secondary
-    "8.8.8.8" # Google
-  ];
-}
+
+  networking = {
+    # Turn off the router automatic DNS:
+    networkmanager.dns = "none";
+
+    # DNS Servers available to the system:
+    nameservers = [
+      "1.1.1.1" # Cloudflare Primary
+      "1.0.0.1" # Cloudflare Secondary
+      "8.8.8.8" # Google
+    ];
+  };
+  }
