a403452cb7413d16b775fd05cff46dc04a94e72f
Connor Garey <connorgarey1999@gmail.com>
2026-01-19 23:35:32 +0000

    Added SearXNG port

--- user-preferences/networking.nix
+++ user-preferences/networking.nix
@@ -14,12 +14,14 @@
         30000 # Luanti
         53317 # Localsend
         40050 # Computer-stat-net
-        5069 # System Shock 2
+        5069  # System Shock 2
+        8080  # SearXNG 
       ];
       UDP = [
         30000 # Luanti
         53317 # Localsend
-        5069 # System Shock 2
+        5069  # System Shock 2
+        8080  # SearXNG 
       ];
     };
   };
