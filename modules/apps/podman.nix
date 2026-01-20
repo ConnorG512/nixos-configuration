99fc9422cf5ff8222a72b267ee426aa21e15da63
Connor Garey <connorgarey1999@gmail.com>
2026-01-20 13:37:05 +0000

    Removed wrappers.

--- modules/apps/podman.nix
+++ modules/apps/podman.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, pkgs, ... }:
 
 {
   virtualisation = {
@@ -7,21 +7,6 @@
       enable = true;
       dockerCompat = true;
       defaultNetwork.settings.dns_enabled = true;
-    };
-  };
-
-  security.wrappers = {
-    newuid = {
-      owner = "root";
-      group = "root";
-      source = "${pkgs.su}/bin/newuidmap";
-      setuid = true;
-    };
-    newgid = {
-      owner = "root";
-      group = "root";
-      source = "${pkgs.su}/bin/newgidmap";
-      setuid = true;
     };
   };
 }
