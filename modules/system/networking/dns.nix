c1d5f9a71794ee711a3cffd4910682a9603982e3
Connor Garey <connorgarey1999@gmail.com>
2026-01-21 13:00:08 +0000

    Removed force off for resolved

--- modules/system/networking/dns.nix
+++ modules/system/networking/dns.nix
@@ -7,9 +7,6 @@ in
   # Currently auto detecting DNS from router however sometimes failing and cutting internet.
   # Force use of public DNS addresses and disable auto resolving.
   
-  # resolved is currently forced off, set to true or remove to turn on. 
-  services.resolved.enable = lib.mkForce false; 
-
   networking = {
     # Turn off the router automatic DNS:
     networkmanager.dns = "none";
