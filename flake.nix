0278dfb00a91a8f1b0d57f03b3aed39ef2554470
Connor Garey <connorgarey1999@gmail.com>
2026-01-22 23:18:29 +0000

    fixed url

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     nvf.url = "github:notashelf/nvf";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    fastfetch.url = "github:ConnorG512/nff";
+    nff.url = "github:ConnorG512/nff";
   };
 
   outputs =
