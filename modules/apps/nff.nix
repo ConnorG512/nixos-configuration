d835c26759b13ab5f919bfce85d1bea8e026dce8
Connor Garey <connorgarey1999@gmail.com>
2026-01-23 18:43:20 +0000

    nff config

--- modules/apps/nff.nix
+++ modules/apps/nff.nix
@@ -6,19 +6,52 @@
 
     settings = {
       logo = {
+        type = "builtin";
+        source = "auto";
+
         padding = {
-          top = 4;
-          left = 4;
-          right = 4;
+          top = 2;
+          left = 0;
+          right = 2;
         };
       };
+
+      display = {
+        
+        percent = {
+          type = 9;
+          color = {
+            green = "green";
+            yellow = "light_yellow";
+            red = "light_red";
+          };
+        };
+        
+        bar = {
+          width = 12;
+        };
+      };
+
       modules = [
         "title"
         "separator"
-        "title"
-        "separator"
         "os"
+        "host"
+        "uptime"
         "separator"
+        "cpu"
+        "separator"
+        "gpu"
+        "vulkan"
+        "opengl"
+        "opencl"
+        "separator"
+        "memory"
+        "separator"
+        "disk"
+        "separator"
+        "network"
+        "localip"
       ];
     };
   };
