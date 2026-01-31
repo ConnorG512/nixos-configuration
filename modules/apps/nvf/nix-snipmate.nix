62a30dfe3753de8d8996d600b5dffc2c3a268746
Connor Garey <connorgarey1999@gmail.com>
2026-01-31 15:01:23 +0000

    Added configOptionPreset

--- modules/apps/nvf/nix-snipmate.nix
+++ modules/apps/nvf/nix-snipmate.nix
@@ -10,4 +10,29 @@
       }
     '';
   }
+
+  {
+    trigger = "configOptionPreset";
+    body = ''
+      { config, lib, ... }:
+
+      let
+        cfg = config.$1
+      in
+      {
+        options.$1 = {
+          enable = lib.mkOption {
+            type = lib.types.bool;
+            default = false;
+            description = "enable option.";
+            example = true;
+          };
+        };
+
+        config = lib.mkIf cfg.enable {
+          # Do things.
+        };
+      }
+    '';
+  }
 ]
