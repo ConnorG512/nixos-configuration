de01786b0a2708a574070badb5d538b002a9d335
Connor Garey <connorgarey1999@gmail.com>
2026-01-31 16:08:46 +0000

    Added multiple nix templates

--- modules/apps/nvf/nix-snipmate.nix
+++ modules/apps/nvf/nix-snipmate.nix
@@ -1,13 +1,15 @@
 [
   {
-    trigger = "mkOption";
+    trigger = "linuxPkgsX86_64";
     body = ''
-      mkOption {
-        type = $1;
-        default = $2;
-        description = $3;
-        example = $4;
-      }
+      $1 = nixpkgs.legacyPackages.x86_64-linux
+    '';
+  }
+
+  {
+    trigger = "linuxMingwW64Pkgs";
+    body = ''
+      $1 = nixpkgs.legacyPackages.x86_64-linux.pkgsCross.mingwW64
     '';
   }
 
@@ -35,15 +37,64 @@
       }
     '';
   }
+  
+  {
+    trigger = "mkOption";
+    body = ''
+      mkOption {
+        type = $1;
+        default = $2;
+        description = $3;
+        example = $4;
+      }
+    '';
+  }
 
   {
     trigger = "mkDerivation";
     body = ''
       stdenv.mkDerivation (finalAttrs: {
         pname = "$1";
-        version = "$2"
+        version = "$2";
         src = $3;
+
+        dontStrip = $4;
+
+        nativeBuildInputs = [ ];
+        buildInputs = [ ];
       });
+    '';
+  }
+
+  {
+    trigger = "buildPhase";
+    body = ''
+      buildPhase = ""
+        runHook preBuild
+        $0
+        runHook postBuild
+      "";
+    '';
+  }
+  
+  {
+    trigger = "installPhase";
+    body = ''
+      installPhase = ""
+        runHook preInstall
+        $0
+        runHook postInstall
+      "";
+    '';
+  }
+  
+  {
+    trigger = "cmakeFlags"; 
+    body = ''
+      cmakeFlags = [
+        "-DCMAKE_BUILD_TYPE=$1"
+        $0
+      ];
     '';
   }
 
@@ -51,14 +102,43 @@
     trigger = "mkShell";
     body = ''
       devShells.$1.default = $2.mkShell {
-        packages = with $2; [
-          
-        ];
+        packages = [ ];
 
-        shellHook = ''
+        shellHook = ""
           echo "Entering shell!"
-        '';
+        "";
       }
+    '';
+  }
+
+  {
+    trigger = "wrapProgram";
+    body = ''
+      wrapProgram $1 \
+        $2
+    '';
+  }
+
+  {
+    trigger = "importsList";
+    body = ''
+      imports = [
+        $0
+      ];
+    '';
+  }
+  
+  {
+    trigger = "import";
+    body = ''
+      $1 = import $2;
+    '';
+  }
+
+  {
+    trigger = "importAndAppend";
+    body = ''
+      $1 = (import $2).$3;
     '';
   }
 ]
