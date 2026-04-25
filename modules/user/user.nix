ea4ea5740519aebbb92c72f57058358159aad8ed
Connor Garey <connorgarey1999@gmail.com>
2026-04-25 16:01:46 +0100

    options for user module

--- modules/user/user.nix
+++ modules/user/user.nix
@@ -18,15 +18,47 @@ in
       description = "List of groups to provide to the user.";
       example = [ "wheel" "video" ];
     };
-  };
 
-  config = {
-    users.users.${cfg.name} = {
-      isNormalUser = true;
-      extraGroups = cfg.groupList;
-      initialPassword = "changeme";
-      shell = pkgs.zsh;
+    useZsh = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Use ZShell";
+      example = false;
+    };
+
+    useHMDotfiles = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Use Home Manager to manager dotfiles.";
+      example = true;
+    };
+
+    userPackageList = lib.mkOption {
+      type = lib.types.listOf lib.types.package;
+      default = [];
+      description = "Packages to install on a user level.";
+      example = with pkgs; [ firefox mpv ];
     };
   };
+
+  config = lib.mkMerge [
+    {
+
+      users.users.${cfg.name} = {
+        isNormalUser = true;
+        extraGroups = cfg.groupList;
+        initialPassword = "changeme";
+        shell = pkgs.zsh;
+      };
+    }
+
+    (lib.mkIf cfg.useHMDotfiles {
+      home-manager.users.${cfg.name} = {
+        home.stateVersion = "24.11";
+        programs.zsh.enable = cfg.useZsh;
+        home.packages = cfg.userPackageList;
+      };
+    })
+  ];
 }
 
