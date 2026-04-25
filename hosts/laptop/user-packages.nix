8fdab83d2fcf50e227705b2668e9d78507a3f6fa
Connor Garey <connorgarey1999@gmail.com>
2026-04-25 15:50:40 +0100

    Added a user-packages module for laptop packages.

---
+++ hosts/laptop/user-packages.nix
@@ -0,0 +1,27 @@
+{ consfig, pkgs, lib, ... }:
+
+with pkgs; [
+  cava
+  qbittorrent
+  xxd
+
+  git
+  lazygit
+  scanmem
+  strace
+  cloc
+
+  luanti
+  prismlauncher
+
+  firefox-bin
+  tor-browser
+
+  libreoffice-qt6-fresh
+
+  spotify
+  termusic
+  mpv
+  
+  discord
+]
