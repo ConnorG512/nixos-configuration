6b09576eca2332c9c6ea0de4341cae66f5dc6bf7
Connor Garey <connorgarey1999@gmail.com>
2026-04-25 14:13:21 +0100

    Added localsend

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -37,6 +37,11 @@
     installWinePackages = true;
   };
 
+  programs.localsend = {
+    enable = true;
+    openFirewall = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
