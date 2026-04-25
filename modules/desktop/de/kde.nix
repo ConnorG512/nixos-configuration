a57be1ecdc4efc099d151c8084ece6903cacb1cb
Connor Garey <connorgarey1999@gmail.com>
2026-04-25 21:04:12 +0100

    removed systempackages from KDE

--- modules/desktop/de/kde.nix
+++ modules/desktop/de/kde.nix
@@ -5,12 +5,4 @@
     enable = true;
     enableQt5Integration = true;
   };
-
-  environment.systemPackages = with pkgs; [
-    firefox 
-    kdePackages.kcalc
-    kdePackages.gwenview
-    kdePackages.okular
-    kdePackages.elisa
-  ];
 }
