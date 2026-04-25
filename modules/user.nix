45961bff76b59e364a8af313a3b2d93f478883a9
Connor Garey <connorgarey1999@gmail.com>
2026-04-25 17:50:36 +0100

    removed option to use HM and use it by default.

--- modules/user.nix
+++ modules/user.nix
@@ -26,13 +26,6 @@ in
       example = false;
     };
 
-    useHMDotfiles = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Use Home Manager to manager dotfiles.";
-      example = true;
-    };
-
     userPackageList = lib.mkOption {
       type = lib.types.listOf lib.types.package;
       default = [];
@@ -50,23 +43,22 @@ in
     };
   };
 
-  config = lib.mkMerge [
-    {
+  config = {
       users.users.${cfg.name} = {
         isNormalUser = true;
         extraGroups = cfg.groupList;
         initialPassword = "changeme";
         shell = pkgs.zsh;
       };
-    }
 
-    (lib.mkIf cfg.useHMDotfiles {
-      home-manager.users.${cfg.name} = {
-        home.stateVersion = "24.11";
-        programs.zsh.enable = cfg.useZsh;
-        home.packages = cfg.userPackageList;
+    home-manager.users.${cfg.name} = {
+      home.stateVersion = "24.11";
+      programs.zsh.enable = cfg.useZsh;
+      home = {
+        packages = cfg.userPackageList;
+        file = cfg.configFileList;
       };
-    })
-  ];
+    };
+  };
 }
 
