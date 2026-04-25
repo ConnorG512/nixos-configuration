9b6624563c97836433fddf74b3d0c8ae231d03dd
Connor Garey <connorgarey1999@gmail.com>
2026-04-25 13:25:50 +0100

    created options for system packages based on x11 or wl use.

--- modules/system/system-packages.nix
+++ modules/system/system-packages.nix
@@ -1,24 +1,45 @@
-{config, pkgs, ... }:
-let 
-  codecs = import ./multimedia-codec.nix { inherit pkgs; };
-in
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.systemConfiguration.sysPackages;
+in 
 {
-  # Core system packages installed to all users.
-  environment.systemPackages = with pkgs; [
-    fuse3
-    exfat
-    nixfmt-rfc-style
-    wl-clipboard
-    wlr-randr
-    fd
-    gamescope
-    su
-    shadow
-    nil
-    nix-index
-    bash-language-server
-    exiftool
-  ]
-  ++ codecs.gstreamerCodecs
-  ++ codecs.ffmpegCodecs; 
+  options.systemConfiguration.sysPackages = {
+    displayType = lib.mkOption {
+      type = lib.types.enum [ "x11" "wl" ];
+      default = "wl";
+      description = "Whether to install system packages for x11 or wayland protocols";
+    };
+  };
+
+  config = lib.mkMerge [
+    # Wayland defaults:
+    (lib.mkIf (cfg.displayType == "wl"){
+      environment.systemPackages = with pkgs; [
+        wlrandr
+        wl-clipboard
+      ];
+    })
+    
+    # X11 Defaults:
+    (lib.mkIf (cfg.displayType == "x11"){
+      environment.systemPackages = with pkgs; [
+        xrandr
+        xeyes
+      ];
+    })
+
+    # Common: 
+    {
+      environment.systemPackages = with pkgs; [
+        fuse3
+        exfat
+        fd
+        su
+        shadow
+        nix-index
+        exiftool
+      ];
+    }
+  ];
 }
