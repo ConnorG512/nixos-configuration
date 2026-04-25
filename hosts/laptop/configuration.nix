3fa3e3d19cebcbdf7f5361c73bac9da83c37dd2c
Connor Garey <connorgarey1999@gmail.com>
2026-04-25 18:43:45 +0100

    Adde controllers to laptop

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -25,6 +25,7 @@
     "${self}/modules/system/audio/pipewire.nix"
     "${self}/modules/system/system-packages.nix"
     "${self}/modules/system/shell/zsh.nix"
+    "${self}/modules/system/controllers.nix"
 
     "${self}/modules/system/networking/firewall.nix"
     
