6ee3883b6795bd8b4ba284e6ab5f1f5cc2b28bf7
Connor Garey <connorgarey1999@gmail.com>
2026-04-25 21:12:54 +0100

    basic options for kde

--- modules/desktop/de/kde.nix
+++ modules/desktop/de/kde.nix
@@ -1,8 +1,29 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
+let
+  cfg = config.systemConfiguration.desktop.kde;
+in 
 {
-  services.desktopManager.plasma6 = {
-    enable = true;
-    enableQt5Integration = true;
+  options.systemConfiguration.desktop.kde = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable KDE";
+      example = true;
+    };
+
+    useQt5 = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable Qt5 intergration.";
+      example = true;
+    };
+  };
+  
+  config = {
+    services.desktopManager.plasma6 = {
+      enable = cfg.enable;
+      enableQt5Integration = cfg.useQt5;
+    };
   };
 }
