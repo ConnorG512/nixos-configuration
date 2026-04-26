d7eb9d20a3dd6056f007b6acf3df3669b81de8a9
Connor Garey <connorgarey1999@gmail.com>
2026-04-26 15:12:59 +0100

    Removed hyprland config files

--- hosts/desktop/config-files.nix
+++ hosts/desktop/config-files.nix
@@ -5,15 +5,6 @@
     ".config/alacritty/alacritty.toml" = "${self}/dotfiles/.config/alacritty/alacritty.toml";
     ".config/fastfetch/config.jsonc" = "${self}/dotfiles/.config/fastfetch/config.jsonc";
 
-    ".config/hypr/hyprland-animations.conf" = "${self}/dotfiles/.config/hypr/hyprland-animations.conf";
-    ".config/hypr/hyprland-autostart.conf" = "${self}/dotfiles/.config/hypr/hyprland-autostart.conf";
-    ".config/hypr/hyprland-keybinds.conf" = "${self}/dotfiles/.config/hypr/hyprland-keybinds.conf";
-    ".config/hypr/hyprland-monitors.conf" = "${self}/dotfiles/.config/hypr/hyprland-monitors.conf";
-    ".config/hypr/hyprland-windows.conf" = "${self}/dotfiles/.config/hypr/hyprland-windows.conf";
-    ".config/hypr/hyprland.conf" = "${self}/dotfiles/.config/hypr/hyprland.conf";
-    ".config/hypr/hyprlock.conf" = "${self}/dotfiles/.config/hypr/hyprlock.conf";
-    ".config/hypr/hyprpaper.conf" = "${self}/dotfiles/.config/hypr/hyprpaper.conf";
-
     ".config/tofi/config" = "${self}/dotfiles/.config/tofi/config";
     ".config/waybar/config.jsonc" = "${self}/dotfiles/.config/waybar/config.jsonc";
     ".config/waybar/style.css" = "${self}/dotfiles/.config/waybar/style.css";
