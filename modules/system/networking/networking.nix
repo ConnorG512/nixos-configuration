a4315f8db1940c4a00fa47060856d177b2169024
Connor Garey <connorgarey1999@gmail.com>
2026-04-27 13:38:58 +0100

    Missing brace.

--- modules/system/networking/networking.nix
+++ modules/system/networking/networking.nix
@@ -86,13 +86,14 @@ in
       services.avahi = {
       enable = true;
       nssmdns = true;
-      publish = {
-        enable = true;
-        addresses = true;
-        domain = true;
-        hinfo = true;
-        userServices = true;
-        workstation = true;
+        publish = {
+          enable = true;
+          addresses = true;
+          domain = true;
+          hinfo = true;
+          userServices = true;
+          workstation = true;
+        };
       };
     })
   ];
