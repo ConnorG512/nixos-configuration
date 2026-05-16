6fd63b7732c963ec28da63cb796deba0eef37884
Connor Garey <connorgarey1999@gmail.com>
2026-05-16 18:10:07 +0100

    Use options for Niri

--- modules/desktop/wm/niri.nix
+++ modules/desktop/wm/niri.nix
@@ -1,43 +1,68 @@
-{ pkgs, config, ... }:
+{ pkgs, config, lib, ... }:
 
+let
+  cfg = config.systemConfiguration.desktop;
+in 
 {
-  programs.niri = {
-    enable = true;
-    useNautilus = true;
-  };
-  
-  environment.systemPackages = with pkgs; [
-    xdg-desktop-portal-gtk
-    xdg-desktop-portal-gnome
-    xwayland-satellite
-    mesa
+  options.systemConfiguration.desktop.niri = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable the use of Niri";
+      example = true;
+    };
     
-    hyprlock
-    hyprpaper
-    mako
-    nautilus
-    gsettings-desktop-schemas
-    waybar
-    alacritty
-    grim
-    slurp
-    fuzzel
-  ];
-
-  programs.nautilus-open-any-terminal = {
-    enable = true; 
-    terminal = "alacritty";
-  };
-  
-  services = {
-    gvfs.enable = true;
-    gnome = {
-      gnome-keyring.enable = true;
-      sushi.enable = true;
+    enableSatellite = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable Xwayland-Satellite";
+      example = true;
     };
   };
 
-  xdg.portal = {
-    enable = true;
+  config = {
+    programs.niri = {
+      enable = cfg.niri.enable;
+      useNautilus = cfg.niri.enable;
+    };
+    
+    environment.systemPackages = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-gnome
+      
+      hyprlock
+      hyprpaper
+      mako
+      nautilus
+      gsettings-desktop-schemas
+      waybar
+      alacritty
+      grim
+      slurp
+      fuzzel
+    ];
+
+    (lib.mkIf cfg.niri.enableSatellite {
+      environment.systemPackages = with pkgs; [
+        xwayland-satellite
+      ];
+    })
+
+    programs.nautilus-open-any-terminal = {
+      enable = cfg.niri.enable; 
+      terminal = "alacritty";
+    };
+    
+    services = {
+      gvfs.enable = cfg.niri.enable;
+      gnome = {
+        gnome-keyring.enable = cfg.niri.enable;
+        sushi.enable = cfg.niri.enable;
+      };
+    };
+
+    xdg.portal = {
+      enable = cfg.niri.enable;
+    };
   };
 }
