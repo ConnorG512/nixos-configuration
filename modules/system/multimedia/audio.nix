230b20819dc9a9d3ec139a4cea63ca83ac5a2d78
Connor Garey <connorgarey1999@gmail.com>
2026-05-28 17:22:26 +0100

    Audio Rewrite.
    Created an option for hiding a list of given ports.
    
    Empty list as a default.
    
    fix str
    
    .
    
    Loopback / Sink rewrite required
    
    createLoopback lambda
    
    removed let ... in
    
    Create sink
    
    Audio Rewrite

--- modules/system/multimedia/audio.nix
+++ modules/system/multimedia/audio.nix
@@ -1,129 +1,206 @@
-{ config, lib, pkgs, ... }: 
+{ config
+, lib
+, pkgs
+, ...
+}:
 
 let
   cfg = config.systemConfiguration.multimedia.audio;
-in 
+
+  # Functions:
+  enablePlugins =
+    pluginList:
+    lib.genAttrs pluginList (name: {
+      enable = true;
+    });
+
+in
 {
   options.systemConfiguration.multimedia.audio = {
     plugins = lib.mkOption {
-      type = lib.types.listOf (lib.types.enum [ "audio" "jack" "pulse" "alsa" ]);
-      default = [];
+      type = lib.types.listOf (
+        lib.types.enum [
+          "audio"
+          "jack"
+          "pulse"
+          "alsa"
+        ]
+      );
+      default = [
+        "audio"
+        "jack"
+        "pulse"
+        "alsa"
+      ];
       description = "Pipewire plugins to enable.";
       example = false;
     };
-    
+
     clockRate = lib.mkOption {
       type = lib.types.int;
       default = 44100;
       description = "Pipewire audio clock rate.";
       example = 48000;
     };
+
+    hiddenPorts = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+      description = "List of full node.name ports to hide in wireplumber.";
+      example = [ "alsa_output.pci-0000_08_00.1.pro-output-0" ];
+    };
+
+    renamedPorts = lib.mkOption {
+      type = lib.types.listOf lib.types.attrs;
+      default = [ ];
+      description = "list of attributes of devices to rename.";
+      example = [
+        {
+          target = "alsa_output.pci-0000_08_00.1.pro-output-0";
+          newName = "Headset";
+        }
+      ];
+    };
+
+    # Custom modules:
+    # Finding ID number can be done with pwctl
+    # Finding the correct string for target.object can be found with pw-cli info
+    # Combine Stream: https://docs.pipewire.org/page_module_combine_stream.html
+    loopbackModules = lib.mkOption {
+      type = lib.types.listOf lib.types.attrs;
+      default = [ ];
+      description = "List of pipewire modules to create";
+    };
+    sinkModules = lib.mkOption {
+      type = lib.types.listOf lib.types.attrs;
+      default = [ ];
+      description = "List of pipewire modules to create";
+    };
   };
 
-    config = lib.mkIf (cfg.plugins != []) (lib.mkMerge [
-    {
-      services.pipewire = lib.genAttrs cfg.plugins (name: {
-        enable = true;
-      });
-    }
+  config = lib.mkIf (cfg.plugins != [ ]) (
+    lib.mkMerge [
 
-    {
-      services.pipewire = {
-        extraConfig.pipewire = {
+      # Globals:
+      {
+        services.pipewire.extraConfig.pipewire = {
           "context.properties" = {
             "default.clock.rate" = cfg.clockRate;
           };
-
-          # Custom modules: 
-          
-          # Finding ID number can be done with pwctl
-          # Finding the correct string for target.object can be found with pw-cli info 
-          "90-discord-splitter" = {
-            "context.modules" = [
-              {
-                name = "libpipewire-module-loopback";
-                args = {
-                  "audio.position" = [ "FL" "FR" ];
-                  "capture.props" = {
-                    "media.class" = "Audio/Sink";
-                    "node.name" = "discord_splitter";
-                    "node.description" = "Sink for splitting Discord capture audio";
-                  };
-                  "playback.props" = {
-                    "node.name" = "discord_splitter.output";
-                    "node.passive" = true;
-                    "target.object" = "alsa_output.pci-0000_08_00.1.pro-output-10";
-                  };
-                };
-              }
-            ];
-          };
-          "91-headphones-sink" = {
-            "context.modules" = [
-              {
-                name = "libpipewire-module-loopback";
-                args = {
-                  "audio.position" = [ "FL" "FR" ];
-                  "capture.props" = {
-                    "media.class" = "Audio/Sink";
-                    "node.name" = "headphones_sink";
-                    "node.description" = "Sink for passing audio to headphones";
-                  };
-                  "playback.props" = {
-                    "node.name" = "headphones_sink.output";
-                    "node.passive" = true;
-                    "target.object" = "alsa_output.pci-0000_08_00.1.pro-output-10";
-                  };
-                };
-              }
-            ];
-          };
-          "92-tv-sink" = {
-            "context.modules" = [
-              {
-                name = "libpipewire-module-loopback";
-                args = {
-                  "audio.position" = [ "FL" "FR" ];
-                  "capture.props" = {
-                    "media.class" = "Audio/Sink";
-                    "node.name" = "tv_sink";
-                    "node.description" = "Sink for passing audio to TV";
-                  };
-                  "playback.props" = {
-                    "node.name" = "tv_sink.output";
-                    "node.passive" = true;
-                    "target.object" = "alsa_output.pci-0000_08_00.1.pro-output-3";
-                  };
-                };
-              }
-            ];
-          };
-
-          # Combine Stream: https://docs.pipewire.org/page_module_combine_stream.html
-          "93-tv-headphones-sink" = {
-            "context.modules" = [
-              {
-                name = "libpipewire-module-combine-stream";
-                args = {
-                  "combine.mode" = "sink";
-                  "node.name" = "tv-headphones-combine-sink";
-                  "node.description" = "Connect audio to headphones and TV";
-                  "combine.props" = {
-                    "audio.position" = [ "FL" "FR" ];
-                  };
-                  "stream.props" = {
-                    "stream.dont-remix" = true;
-                  };
-                  "targets" = [
-                    { "target.object" = "alsa_output.pci-0000_08_00.1.pro-output-10"; }
-                    { "target.object" = "alsa_output.pci-0000_08_00.1.pro-output-3"; }
-                  ];
-                };
-              }
-            ];
-          };
         };
-      };
-    }
-  ]);
+      }
+
+      # Enable modules:
+      {
+        services.pipewire =
+          let
+            enableServices =
+              serviceList:
+              lib.genAttrs serviceList
+                (service: {
+                  enable = true;
+                });
+          in
+          enableServices cfg.plugins;
+      }
+
+      # Pipewire Modules:
+      {
+        services.pipewire.extraConfig.pipewire =
+          let
+            createLoopbacks =
+              loopbackList:
+              lib.mergeAttrsList
+                (builtins.map (
+                  { fileName
+                  , nodeName
+                  , nodeDescription
+                  , targetOutput
+                  ,
+                  }:
+                  {
+                    "${fileName}" = {
+                      name = "libpipewire-module-loopback";
+                      args = {
+                        "audio.position" = [
+                          "FL"
+                          "FR"
+                        ];
+                        "capture-props" = {
+                          "media.class" = "Audio/Sink";
+                          "node.name" = nodeName;
+                          "node.description" = nodeDescription;
+                        };
+                        "playback-props" = {
+                          "node.name" = "${nodeName}.output";
+                          "node.passive" = true;
+                          "target.object" = targetOutput;
+                        };
+                      };
+                    };
+                  }
+                )loopbackList);
+
+            createSinks =
+              sinkList:
+              lib.mergeAttrsList
+                (builtins.map (
+                  { fileName
+                  , nodeName
+                  , nodeDescription
+                  , targets
+                  ,
+                  }:
+                  {
+                    "${fileName}" = {
+                      name = "libpipewire-module-combine-stream";
+                      args = {
+                        "combine.mode" = "sink";
+                        "node.name" = nodeName;
+                        "node.description" = nodeDescription;
+                        "combine.props" = {
+                          "audio.position" = [
+                            "FL"
+                            "FR"
+                          ];
+                        };
+                        "stream.props" = {
+                          "stream.dont.remix" = true;
+                        };
+                        "targets" = builtins.map
+                          (target: {
+                            "target.object" = target;
+                          })
+                          targets;
+                      };
+                    };
+                  }
+                )sinkList);
+          in
+            (if cfg.loopbackModules == [] then {} else createLoopbacks cfg.loopbackModules)
+            // (if cfg.sinkModules == [] then {} else createSinks cfg.sinkModules);
+      }
+
+      # Wireplumber modules:
+      {
+        services.pipewire.wireplumber.extraConfig =
+          let
+          hidePorts = portList: builtins.map (port: {
+            matches = [{ "node.name" = "${port}"; }];
+            actions = { update-props = { "node.disabled" = true; }; }; 
+          }) portList; 
+          
+          renamePorts = portList: builtins.map (port: {
+            matches = [{ "node.name" = "${port.target}"; }];
+            actions = { update-props = { "node.description" = port.newName; }; }; 
+          }) portList; 
+
+          in
+          {
+            "20-rename-port"."monitor-alsa-rules" = renamePorts cfg.renamedPorts;
+            "30-hidden-ports"."monitor-alsa-rules" = hidePorts cfg.hiddenPorts;
+          };
+      }
+    ]
+  );
 }
