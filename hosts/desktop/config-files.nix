bb1986ea1728726b612a26e0dfdfd2f08578dd64
Connor Garey <connorgarey1999@gmail.com>
2026-06-01 20:06:03 +0100

    Created ghostty config.
    Added ghostty path.
    
    .
    
    .
    
    .
    
    Added pallette and changed background.
    
    Darkened green.
    
    .
    
    .
    
    .

--- hosts/desktop/config-files.nix
+++ hosts/desktop/config-files.nix
@@ -3,6 +3,7 @@
 {
     ".zshrc" = "${self}/dotfiles/.zshrc";
     ".config/alacritty/alacritty.toml" = "${self}/dotfiles/.config/alacritty/alacritty.toml";
+    ".config/ghostty/config.ghostty" = "${self}/dotfiles/.config/ghostty/config.ghostty";
     ".config/fastfetch/config.jsonc" = "${self}/dotfiles/.config/fastfetch/config.jsonc";
 
     ".config/tofi/config" = "${self}/dotfiles/.config/tofi/config";
